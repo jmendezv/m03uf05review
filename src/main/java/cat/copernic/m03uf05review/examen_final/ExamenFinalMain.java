5d4c9635bf4a041e9fb661fef233b9e556fa245d
Pep <pep.mendez@copernic.cat>
2020-11-24 18:36:59 +0100

    Examen final is ready

---
+++ src/main/java/cat/copernic/m03uf05review/examen_final/ExamenFinalMain.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cat.copernic.m03uf05review.examen_final;
+
+/**
+ * Crea la classe ProductItem amb les propietats: id, name i description.
+ * 
+ * La classe ProductItem ha d'implementar la interfícia Comparable segons el
+ * camp 'id'.
+ * 
+ * Crea una llista de tipus ProductItem, afegeix 10 elements i ordena-la
+ * per id.
+ * 
+ * Recòrre la llista i escriu els productes amb nom 'ref' seguit d'un número de
+ * tres dígits, per exemple: ref001. Usa una expressió regular.
+ * 
+ * 
+ * @author pep
+ */
+public class ExamenFinalMain {
+    
+}
